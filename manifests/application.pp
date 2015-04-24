5a868da3cf4ed83eb9e6b89073b4a453ffff5763
Pierre Gambarotto <pierre.gambarotto@n7.fr>
2015-04-24 11:05:08 +0200

    ensure creation of .ssh directory

--- manifests/application.pp
+++ manifests/application.pp
@@ -1,7 +1,6 @@
 # dependancies : pm2, ssh_authorized_keys
 
 define pm2::application(
-  require pm2
   $app_name = $name,
   $username = $app_name,
   $directory = "/usr/local/${app_name}",
@@ -9,7 +8,7 @@ define pm2::application(
   $ssh_login_keytype = undef,
   $ssh_login_pubkey = undef
   ){
-
+  require pm2
   # user to run the service
   user{$username:
     ensure => present,
@@ -17,7 +16,14 @@ define pm2::application(
     home => $directory,
     shell => '/bin/bash',
     managehome => true,
-  }
+  } ->
+  file{"${directory}/.ssh":
+    ensure => directory,
+    owner => $username,
+    group => $username,
+    mode => 0700
+  }  
+
 
   if ($ssh_login_keytype and $ssh_login_pubkey){
     ssh_authorized_key{"manager of ${app_name}":
